d7c07f009e85e0d3376d5be643a2d126584a7b09
oue123 <oue123@yandex.ru>
2022-07-29 18:29:14 +0300

    clear

--- restik_masterserver/License.cs
+++ restik_masterserver/License.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace restik_masterserver
+﻿namespace restik_masterserver
 {
     internal class License
     {
